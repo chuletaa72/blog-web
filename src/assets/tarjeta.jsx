8a20502818da97afb8107af75c3339831ebacaa6
chuletaa72 <carlos.escalante2002@gmail.com>
2024-05-25 00:19:26 -0600

    Agregando estilos

--- src/assets/tarjeta.jsx
+++ src/assets/tarjeta.jsx
@@ -5,7 +5,7 @@ const card = () => {
     return (
         <div className="presentation-card">
                 <img/>
-                <h2 className="tarjeta-nombre">Mi nombre es Carlos Alberto Escalante Villegas</h2>
+                <h2 className="tarjeta-nombre">Mi nombre es: Carlos Alberto Escalante Villegas</h2>
                 <h2 className="tarjeta-titulo">Me dedico al desarrollo Front-End</h2>
                 <h2 className="tarjeta-correo">Mi correo es: escalante.dev.software@gmail.com</h2>
         </div>
